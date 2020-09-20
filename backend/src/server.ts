001a64d16c4bd769e9c4730af050ce9e369003f9
Rayson Koh <rayson.rk.koh@gmail.com>
2020-09-21 04:14:28 +0800

    Add basic passport local auth

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 require('dotenv').config();
 import express from 'express';
-import { Pool } from 'pg';
+import passport from 'passport';
+import authRoutes from './routes/auth';
 
 const app = express();
+app.use(express.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/auth', authRoutes);
+
 const PORT = process.env.PORT || 3000;
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
-
 app.listen(PORT, () => console.log(`Connected at ${PORT}!`));
