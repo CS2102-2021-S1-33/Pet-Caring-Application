86781e42be786980b7cdaa8c68e162c521f8aed5
Rayson Koh <rayson.rk.koh@gmail.com>
2020-09-21 03:27:24 +0800

    Update tsconfig for build folder

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,8 +9,4 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
-pool.query("SELECT * from game_list", (err, res) => {
-    console.log(err, res);
-})
-
 app.listen(PORT, () => console.log(`Connected at ${PORT}!`));
