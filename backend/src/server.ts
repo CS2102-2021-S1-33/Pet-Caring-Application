7016bfa5c5811116ac1edd438198593740bd267d
Rayson Koh <rayson.rk.koh@gmail.com>
2020-09-21 03:23:43 +0800

    Set up basic connection to postgresql db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,16 @@
 require('dotenv').config();
 import express from 'express';
+import { Pool } from 'pg';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+pool.query("SELECT * from game_list", (err, res) => {
+    console.log(err, res);
+})
 
 app.listen(PORT, () => console.log(`Connected at ${PORT}!`));
