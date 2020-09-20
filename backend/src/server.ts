01bd9605fca910bbc07bab9e897bbd6e9fd0bcea
Rayson Koh <rayson.rk.koh@gmail.com>
2020-09-21 04:27:30 +0800

    Add google style guide linting

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,14 @@
-require('dotenv').config();
-import express from 'express';
-import passport from 'passport';
-import authRoutes from './routes/auth';
+require("dotenv").config();
+import express from "express";
+import passport from "passport";
+import authRoutes from "./routes/auth";
 
 const app = express();
 app.use(express.json());
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Connected at ${PORT}!`));
