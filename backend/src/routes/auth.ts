671ce0b2519c4e2dee5172357aaff30f306ec0ce
Rayson Koh <rayson.rk.koh@gmail.com>
2020-09-21 23:48:29 +0800

    Revert "Add google style guide linting"
    This reverts commit 01bd9605fca910bbc07bab9e897bbd6e9fd0bcea.

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,48 +1,40 @@
-import express from "express";
-import passport from "passport";
-import passportLocal from "passport-local";
-import pool from "../db/init";
+import express from 'express';
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import pool from '../db/init';
 
 const authRoutes = express.Router();
 
-passport.use(
-  new passportLocal.Strategy((username: string, password: string, done) => {
-    pool.query(
-      "SELECT * FROM username_password WHERE username=$1 AND password=$2",
-      [username, password],
-      (error, result) => {
-        if (error) {
-          return done(error);
-        } else {
-          return done(null, result.rows[0]);
-        }
-      }
-    );
-  })
-);
-
-passport.serializeUser(({username, password}, cb) => {
-  cb(null, username);
-});
-
-passport.deserializeUser((username, cb) => {
-  pool.query(
-    "SELECT * FROM username_password WHERE username=$1",
-    [username],
-    (error, result) => {
-      if (error) {
-        return cb(error);
-      } else {
-        return cb(null, result.rows[0]);
-      }
+passport.use(new passportLocal.Strategy(
+    (username: string, password: string, done) => {
+        pool.query('SELECT * FROM username_password WHERE username=$1 AND password=$2', [username, password], (error, result) => {
+            if (error) {
+                return done(error);
+            } else {
+                return done(null, result.rows[0]);
+            }
+        })
     }
-  );
+));
+
+passport.serializeUser(function ({ username, password }, cb) {
+    cb(null, username);
 });
 
-authRoutes.post("/login", passport.authenticate("local"), (req, res) => {
-  res.json({
-    user: req.user,
-  });
+passport.deserializeUser(function (username, cb) {
+    pool.query('SELECT * FROM username_password WHERE username=$1', [username], (error, result) => {
+        if (error) {
+            return cb(error);
+        } else {
+            return cb(null, result.rows[0]);
+        }
+    })
 });
+
+authRoutes.post('/login', passport.authenticate('local'), (req, res) => {
+    res.json({
+        user: req.user
+    })
+})
 
 export default authRoutes;
