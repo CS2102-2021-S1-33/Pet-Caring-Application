09caa692b94a5624ae5997170e20bab3cd785e27
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-02 02:51:46 +0800

    Fix route api path

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -44,7 +44,7 @@ const options = {
     },
   },
   // Path to the API docs
-  apis: [path.join(__dirname, "./routes/*.ts")],
+  apis: ["./src/routes/*.ts"],
 };
 
 // Initialize swagger-jsdoc -> returns validated swagger spec in json format
