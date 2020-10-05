9f1ef8022a1163c533826c66db68d5ee30c6d7ac
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-05 11:56:25 +0800

    Fix bug in authMiddleware return function

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,12 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 
-const authMiddleware = () => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/");
-  };
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(400).json({ msg: "user not authenticated" });
 };
 
 export default authMiddleware;
