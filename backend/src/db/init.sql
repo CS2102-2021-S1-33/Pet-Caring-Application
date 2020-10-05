2dceb0320af7d9649b3ceae20647393d968b010f
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-05 10:51:25 +0800

    Update sql init script with pet and pet categories table

--- backend/src/db/init.sql
+++ backend/src/db/init.sql
@@ -1,21 +1,48 @@
-CREATE TABLE pcs_admin (
+DROP TABLE IF EXISTS owned_pets;
+DROP TABLE IF EXISTS pet_categories;
+DROP TABLE IF EXISTS pcs_admins;
+DROP TABLE IF EXISTS pet_owners;
+DROP TABLE IF EXISTS part_time_caretakers;
+DROP TABLE IF EXISTS full_time_caretakers;
+
+CREATE TABLE pcs_admins (
     username VARCHAR PRIMARY KEY,
     password VARCHAR NOT NULL
 );
 
 CREATE TABLE pet_owners (
     username VARCHAR PRIMARY KEY,
+    email VARCHAR UNIQUE NOT NULL,
+    name VARCHAR NOT NULL,
     password VARCHAR NOT NULL
 );
 
 CREATE TABLE part_time_caretakers (
     username VARCHAR PRIMARY KEY,
+    email VARCHAR UNIQUE NOT NULL,
+    name VARCHAR NOT NULL,
     password VARCHAR NOT NULL
 );
 
 CREATE TABLE full_time_caretakers (
     username VARCHAR PRIMARY KEY,
+    email VARCHAR UNIQUE NOT NULL,
+    name VARCHAR NOT NULL,
     password VARCHAR NOT NULL
+);
+
+CREATE TABLE pet_categories (
+    pet_category_name VARCHAR PRIMARY KEY,
+    set_by VARCHAR NOT NULL REFERENCES pcs_admins(username),
+    base_price INTEGER NOT NULL
+);
+
+CREATE TABLE owned_pets (
+    pet_owner_username VARCHAR REFERENCES pet_owners(username) ON DELETE CASCADE,
+    pet_name VARCHAR,
+    special_requirements VARCHAR,
+    pet_category_name VARCHAR NOT NULL REFERENCES pet_categories(pet_category_name),
+    PRIMARY KEY(pet_owner_username, pet_name)
 );
 
 CREATE VIEW users AS (
@@ -26,7 +53,9 @@ CREATE VIEW users AS (
     SELECT * FROM full_time_caretakers
 );
 
-INSERT INTO pcs_admin VALUES ('admin', 'password');
-INSERT INTO pet_owners VALUES ('sally', 'password');
-INSERT INTO part_time_caretakers VALUES ('john', 'password');
-INSERT INTO full_time_caretakers VALUES ('micky', 'password');
+INSERT INTO pcs_admins VALUES ('admin', 'password');
+INSERT INTO pet_owners VALUES ('sallyPO', 'sally@gmail.com', 'sally chan', 'password');
+INSERT INTO pet_categories VALUES ('dog', 'admin', 10);
+INSERT INTO owned_pets VALUES ('sallyPO', 'doggy', 'cannot eat sweet things', 'dog');
+INSERT INTO part_time_caretakers VALUES ('john', 'john@yahoo.com', 'john tan', 'password');
+INSERT INTO full_time_caretakers VALUES ('micky', 'mick@hotmail.com', 'micky mouse', 'password');
