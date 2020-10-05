14975274136bd5d6814b02a697bb68e2941f4ada
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-05 13:17:32 +0800

    Add admin login route

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -6,6 +6,7 @@ import pool from "../db/init";
 const authRoutes = express.Router();
 
 passport.use(
+  "local-user",
   new passportLocal.Strategy((username: string, password: string, done) => {
     pool.query(
       "SELECT * FROM users WHERE username=$1 AND password=$2",
@@ -21,13 +22,30 @@ passport.use(
   })
 );
 
-passport.serializeUser(function ({ username, password }, cb) {
+passport.use(
+  "local-admin",
+  new passportLocal.Strategy((username: string, password: string, done) => {
+    pool.query(
+      "SELECT * FROM pcs_admins WHERE username=$1 AND password=$2",
+      [username, password],
+      (error, result) => {
+        if (error) {
+          return done(error);
+        } else {
+          return done(null, result.rows[0]);
+        }
+      }
+    );
+  })
+);
+
+passport.serializeUser(({ username }, cb) => {
   cb(null, username);
 });
 
 passport.deserializeUser(function (username, cb) {
   pool.query(
-    "SELECT * FROM users WHERE username=$1",
+    "SELECT * FROM (SELECT username FROM users UNION SELECT username FROM pcs_admins) AS t WHERE username=$1",
     [username],
     (error, result) => {
       if (error) {
@@ -44,7 +62,7 @@ passport.deserializeUser(function (username, cb) {
  *
  * /api/auth/login:
  *   post:
- *     description: Login to the application
+ *     description: Login as a User
  *     produces:
  *       - application/json
  *     consumes:
@@ -68,12 +86,52 @@ passport.deserializeUser(function (username, cb) {
  *       200:
  *         description: Login successful
  */
-authRoutes.post("/login", passport.authenticate("local"), (req, res) => {
+authRoutes.post("/login", passport.authenticate("local-user"), (req, res) => {
   res.json({
     msg: "Found user",
     isAuthenticated: true,
   });
 });
+
+/**
+ * @swagger
+ *
+ * /api/auth/login-admin:
+ *   post:
+ *     description: Login as an Admin
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             username:
+ *               type: string
+ *               example: test
+ *             password:
+ *               type: string
+ *               example: password
+ *           required:
+ *             - username
+ *             - password
+ *     responses:
+ *       200:
+ *         description: Login successful
+ */
+authRoutes.post(
+  "/login-admin",
+  passport.authenticate("local-admin"),
+  (req, res) => {
+    res.json({
+      msg: "Found admin",
+      isAuthenticated: true,
+    });
+  }
+);
 
 /**
  * @swagger
