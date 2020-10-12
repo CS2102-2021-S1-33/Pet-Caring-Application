35fdf91609d74476afe13b1a9a177e6c92f875fe
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-12 13:14:54 +0800

    Format file

--- backend/src/db/init.sql
+++ backend/src/db/init.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS pcs_admins;
 DROP TABLE IF EXISTS pet_owners;
 DROP TABLE IF EXISTS part_time_caretakers;
 DROP TABLE IF EXISTS full_time_caretakers;
+DROP TABLE IF EXISTS verified_caretakers;
 DROP TABLE IF EXISTS availabilities;
 
 CREATE TABLE pcs_admins (
@@ -31,6 +32,28 @@ CREATE TABLE full_time_caretakers (
     email VARCHAR UNIQUE NOT NULL,
     name VARCHAR NOT NULL,
     password VARCHAR NOT NULL
+);
+
+CREATE TABLE pet_categories (
+    pet_category_name VARCHAR PRIMARY KEY,
+    set_by VARCHAR NOT NULL REFERENCES pcs_admins(username),
+    base_price INTEGER NOT NULL
+);
+
+CREATE TABLE owned_pets (
+    pet_owner_username VARCHAR REFERENCES pet_owners(username) ON DELETE CASCADE,
+    pet_name VARCHAR,
+    special_requirements VARCHAR,
+    pet_category_name VARCHAR NOT NULL REFERENCES pet_categories(pet_category_name),
+    PRIMARY KEY(pet_owner_username, pet_name)
+);
+
+CREATE VIEW users AS (
+    SELECT * FROM pet_owners
+    UNION
+    SELECT * FROM part_time_caretakers
+    UNION
+    SELECT * FROM full_time_caretakers
 );
 
 CREATE TABLE verified_caretakers (
@@ -69,66 +92,9 @@ $$ LANGUAGE plpgsql CREATE TRIGGER check_caretaker_exists BEFORE
 
 INSERT ON verified_caretakers FOR EACH ROW EXECUTE PROCEDURE caretaker_exists();
 
-CREATE TABLE pet_categories (
-    pet_category_name VARCHAR PRIMARY KEY,
-    set_by VARCHAR NOT NULL REFERENCES pcs_admins(username),
-    base_price INTEGER NOT NULL
-);
-
-CREATE TABLE owned_pets (
-    pet_owner_username VARCHAR REFERENCES pet_owners(username) ON DELETE CASCADE,
-    pet_name VARCHAR,
-    special_requirements VARCHAR,
-    pet_category_name VARCHAR NOT NULL REFERENCES pet_categories(pet_category_name),
-    PRIMARY KEY(pet_owner_username, pet_name)
-);
-
-CREATE VIEW users AS (
-    SELECT *
-    FROM pet_owners
-    UNION
-    SELECT username,
-        email,
-        name,
-        password
-    FROM part_time_caretakers
-    UNION
-    SELECT username,
-        email,
-        name,
-        password
-    FROM full_time_caretakers
-);
-
-INSERT INTO pcs_admins
-VALUES ('admin', 'password');
-
-INSERT INTO pet_owners
-VALUES (
-        'sallyPO',
-        'sally@gmail.com',
-        'sally chan',
-        'password'
-    );
-
-INSERT INTO pet_categories
-VALUES ('dog', 'admin', 10);
-
-INSERT INTO owned_pets
-VALUES (
-        'sallyPO',
-        'doggy',
-        'cannot eat sweet things',
-        'dog'
-    );
-
-INSERT INTO part_time_caretakers
-VALUES ('john', 'john@yahoo.com', 'john tan', 'password');
-
-INSERT INTO full_time_caretakers
-VALUES (
-        'micky',
-        'mick@hotmail.com',
-        'micky mouse',
-        'password'
-    );
+INSERT INTO pcs_admins VALUES ('admin', 'password');
+INSERT INTO pet_owners VALUES ('sallyPO', 'sally@gmail.com', 'sally chan', 'password');
+INSERT INTO pet_categories VALUES ('dog', 'admin', 10);
+INSERT INTO owned_pets VALUES ('sallyPO', 'doggy', 'cannot eat sweet things', 'dog');
+INSERT INTO part_time_caretakers VALUES ('john', 'john@yahoo.com', 'john tan', 'password');
+INSERT INTO full_time_caretakers VALUES ('micky', 'mick@hotmail.com', 'micky mouse', 'password');
