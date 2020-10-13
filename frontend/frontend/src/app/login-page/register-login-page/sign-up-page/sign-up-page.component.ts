4605edd77a5cdc108db3437de5ee4792f3e8c929
walnads <nad.atiqah@gmail.com>
2020-10-13 15:26:35 +0800

    Redo signup page to match current plans

--- frontend/frontend/src/app/login-page/register-login-page/sign-up-page/sign-up-page.component.ts
+++ frontend/frontend/src/app/login-page/register-login-page/sign-up-page/sign-up-page.component.ts
@@ -1,25 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
+import { FormBuilder, FormGroup, FormArray, Validators, ValidatorFn, FormControl } from "@angular/forms";
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-sign-up-page',
   templateUrl: './sign-up-page.component.html',
-  styleUrls: ['./sign-up-page.component.less']
+  styleUrls: ['sign-up-page.component.less']
 })
+
 export class SignUpPageComponent implements OnInit {
 
   constructor(private registerFormBuilder: FormBuilder) { }
 
-  registerForm = this.registerFormBuilder.group({
-    name: ['', Validators.required],
-    email: ['', Validators.required],
-    address: ['', Validators.required],
-    userName: ['', Validators.required],
-    password: ['', Validators.required],
-    confirmPassword: ['', Validators.required]
-  })
+  registerForm: FormGroup;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.registerForm = this.registerFormBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      address: ['', Validators.required],
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+      typeCaretaker: [''],
+      typePetowner: [''],
+
+      //caretaker fields
+      caretakerRole: [''],
+      //insert dropdown
+
+      //petowner fields
+      noOfPets: [''],
+      pets: new FormArray([])
+
+    });
   }
+
+  onChangePets(e) {
+    const number = e.target.value || 0;
+    if (this.pe.length < number) {
+        for (let i = this.pe.length; i < number; i++) {
+            this.pe.push(this.registerFormBuilder.group({
+                name: ['', Validators.required],
+                //insert dropdown
+                specialReq: ['']
+            }));
+        }
+    } else {
+        for (let i = this.pe.length; i >= number; i--) {
+            this.pe.removeAt(i);
+        }
+    }
+  }
+
+  get f() { return this.registerForm.controls; }
+  get pe() { return this.f.pets as FormArray; }
 
 }
