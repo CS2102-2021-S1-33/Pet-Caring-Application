91ecde26b0600f185b755a967db989325895d937
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-17 09:56:56 +0800

    Add apply leaves table

--- backend/src/db/init.sql
+++ backend/src/db/init.sql
@@ -6,11 +6,16 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO public;
 -- ======================
 
+-- ======================
+-- ADMIN
 CREATE TABLE pcs_admins (
     username VARCHAR PRIMARY KEY,
     password VARCHAR NOT NULL
 );
+-- ======================
 
+-- ======================
+-- PET OWNERS AND PETS
 CREATE TABLE pet_owners (
     username VARCHAR PRIMARY KEY,
     email VARCHAR UNIQUE NOT NULL,
@@ -31,7 +36,10 @@ CREATE TABLE owned_pets (
     pet_category_name VARCHAR NOT NULL REFERENCES pet_categories(pet_category_name),
     PRIMARY KEY(pet_owner_username, pet_name)
 );
+-- ======================
 
+-- ======================
+-- CARETAKERS AND AVAILABILITIES AND LEAVES (FULL-TIME ONLY)
 CREATE TABLE caretakers (
     username VARCHAR PRIMARY KEY,
     email VARCHAR UNIQUE NOT NULL,
@@ -60,33 +68,15 @@ CREATE TABLE advertise_availabilities (
     FOREIGN KEY (ct_username) REFERENCES verified_caretakers(ct_username) ON DELETE CASCADE
 );
 
-CREATE TABLE bid_period (
-    bid_start_period DATE, 
-    bid_end_period DATE, 
-    PRIMARY KEY (bid_start_period, bid_end_period)
-);
-
-CREATE TABLE makes (
-    pet_owner_username VARCHAR,
-    pet_name VARCHAR,
-    bid_start_period DATE, 
-    bid_end_period DATE,
-    ct_username VARCHAR,
-    availability_start_date VARCHAR,
-    availability_end_date VARCHAR,
-    bid_price INTEGER NOT NULL,
+CREATE TABLE apply_leaves (
+    ct_username VARCHAR, 
+    admin_username VARCHAR, 
+    leave_start_date DATE,
+    leave_end_date DATE,
     is_successful BOOLEAN DEFAULT FALSE,
-    payment_method VARCHAR,
-    transfer_method VARCHAR,
-    rating INTEGER,
-    review VARCHAR,
-    PRIMARY KEY (pet_owner_username, pet_name, bid_start_period, bid_end_period, ct_username, availability_start_date, availability_end_date)
-)
-
-CREATE VIEW users AS (
-    SELECT * FROM pet_owners
-    UNION
-    SELECT * FROM caretakers
+    FOREIGN KEY (ct_username) REFERENCES full_time_caretakers(username) ON DELETE CASCADE,
+    FOREIGN KEY (admin_username) REFERENCES pcs_admins(username), 
+    PRIMARY KEY (ct_username, leave_start_date, leave_end_date, admin_username)
 );
 
 CREATE OR REPLACE PROCEDURE add_part_time_caretaker(
@@ -110,7 +100,45 @@ CREATE OR REPLACE PROCEDURE add_full_time_caretaker(
   INSERT INTO full_time_caretakers VALUES (username);
 END;'
 LANGUAGE plpgsql;
+-- ======================
 
+-- ======================
+-- BIDS AND TRANSACTIONS
+CREATE TABLE bid_period (
+    bid_start_period DATE, 
+    bid_end_period DATE, 
+    PRIMARY KEY (bid_start_period, bid_end_period)
+);
+
+CREATE TABLE makes (
+    pet_owner_username VARCHAR,
+    pet_name VARCHAR,
+    bid_start_period DATE, 
+    bid_end_period DATE,
+    ct_username VARCHAR,
+    availability_start_date VARCHAR,
+    availability_end_date VARCHAR,
+    bid_price INTEGER NOT NULL,
+    is_successful BOOLEAN DEFAULT FALSE,
+    payment_method VARCHAR,
+    transfer_method VARCHAR,
+    rating INTEGER,
+    review VARCHAR,
+    PRIMARY KEY (pet_owner_username, pet_name, bid_start_period, bid_end_period, ct_username, availability_start_date, availability_end_date)
+);
+-- ======================
+
+-- ======================
+-- USEFUL VIEWS
+CREATE VIEW users AS (
+    SELECT * FROM pet_owners
+    UNION
+    SELECT * FROM caretakers
+);
+-- ======================
+
+-- ======================
+-- SAMPLE DATA (FOR TESTING)
 INSERT INTO pcs_admins VALUES ('admin', 'password');
 INSERT INTO pet_owners VALUES ('sallyPO', 'sally@gmail.com', 'sally chan', 'password');
 INSERT INTO pet_categories VALUES ('dog', 'admin', 10);
@@ -118,3 +146,4 @@ INSERT INTO owned_pets VALUES ('sallyPO', 'doggy', 'cannot eat sweet things', 'd
 CALL add_part_time_caretaker('john', 'john@yahoo.com', 'john tan', 'password');
 CALL add_full_time_caretaker('micky', 'mick@hotmail.com', 'micky mouse', 'password');
 INSERT INTO verified_caretakers VALUES ('micky', 'admin');
+-- ======================
