a2c76baa66485ad6490d314ed26d391fb5d2c043
Rayson Koh <rayson.rk.koh@gmail.com>
2020-10-17 13:33:09 +0800

    Add check for bid period is subset of availability period

--- backend/src/db/init.sql
+++ backend/src/db/init.sql
@@ -223,20 +223,22 @@ CREATE OR REPLACE PROCEDURE make_bid(
 $$
 DECLARE bid_pet_category VARCHAR; 
 BEGIN
-  -- determine pet category
-  SELECT op.pet_category_name INTO bid_pet_category
-  FROM owned_pets op
-  WHERE op.pet_name = po_pet_name AND op.pet_owner_username = po_username;
+  IF (bid_start_period >= availability_start_date AND bid_end_period <= availability_end_date) THEN
+    -- determine pet category
+    SELECT op.pet_category_name INTO bid_pet_category
+    FROM owned_pets op
+    WHERE op.pet_name = po_pet_name AND op.pet_owner_username = po_username;
 
-  -- check whether bid_price >= daily_price
-  IF EXISTS (
-    SELECT *
-    FROM advertise_for_pet_categories a 
-    WHERE bid_pet_category = a.pet_category_name AND bid_price >= a.daily_price) THEN
+    -- check whether bid_price >= daily_price
+    IF EXISTS (
+      SELECT *
+      FROM advertise_for_pet_categories a 
+      WHERE bid_pet_category = a.pet_category_name AND bid_price >= a.daily_price) THEN
 
-  INSERT INTO bid_period VALUES (bid_start_period, bid_end_period);
-  INSERT INTO makes VALUES (po_username, po_pet_name, bid_start_period, bid_end_period, ct_username, availability_start_date, availability_end_date, bid_price);
+    INSERT INTO bid_period VALUES (bid_start_period, bid_end_period);
+    INSERT INTO makes VALUES (po_username, po_pet_name, bid_start_period, bid_end_period, ct_username, availability_start_date, availability_end_date, bid_price);
 
+    END IF;
   END IF;
 END;
 $$
@@ -278,6 +280,7 @@ CALL advertise_availability('john', DATE '2020-12-01', DATE '2020-12-20', 'dog',
 CALL make_bid('sallyPO', 'petName', DATE '2020-12-01', DATE '2020-12-10', 'john', DATE '2020-12-01', DATE '2020-12-20', 10); -- should insert
 --CALL make_bid('sallyPO', 'petName', DATE '2020-12-01', DATE '2020-12-10', 'john', DATE '2020-12-01', DATE '2020-12-20', 9); -- should insert
 --CALL make_bid('sallyPO', 'testName', DATE '2020-12-01', DATE '2020-12-10', 'john', DATE '2020-12-01', DATE '2020-12-20', 10); -- should NOT insert
+--CALL make_bid('sallyPO', 'petName', DATE '2020-11-01', DATE '2020-12-25', 'john', DATE '2020-12-01', DATE '2020-12-20', 10); -- should NOT insert
 /*
 INSERT INTO pet_owners VALUES ('sallyPO', 'sally@gmail.com', 'sally chan', 'password');
 INSERT INTO pet_categories VALUES ('dog', 'admin', 10);
