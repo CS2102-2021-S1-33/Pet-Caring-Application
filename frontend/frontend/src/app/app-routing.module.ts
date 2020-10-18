e09b9be9eaea6a288663c1f1991c282a86bfcb5a
laurent <55478388+laurentyee@users.noreply.github.com>
2020-10-18 16:18:19 +0800

    landing page html

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { FindCaretakerComponent } from './login-page/find-caretaker/find-caretaker/find-caretaker.component';
 import { RegisterLoginPageComponent } from './login-page/register-login-page/register-login-page/register-login-page.component';
 import {SignUpPageComponent} from "./login-page/register-login-page/sign-up-page/sign-up-page.component";
 
@@ -7,7 +9,9 @@ import {SignUpPageComponent} from "./login-page/register-login-page/sign-up-page
 const routes: Routes = [
   {path: 'login', component: RegisterLoginPageComponent},
   {path:'signup', component: SignUpPageComponent},
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'find-caretaker', component: FindCaretakerComponent},
+  //{path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '', component: IndexComponent}
 ];
 
 @NgModule({
