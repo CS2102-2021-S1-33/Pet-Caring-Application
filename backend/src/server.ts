cdff107d8afef9920b5424cdd3e7265c996456ce
Rayson Koh <rayson.rk.koh@gmail.com>
2020-11-04 13:10:36 +0800

    Fix bug in deploying frontend-testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -40,6 +40,7 @@ if (process.env.NODE_ENV == "production") {
     )
   );
 }
+app.use(express.static(path.join(__dirname, "../../frontend-testing/build/")));
 app.use(
   "/test/frontend-test",
   express.static(path.join(__dirname, "../../frontend-testing/build/"))
